7016e6eff02fe57b86c8257392c21ee28533ec57
svetozar12 <zaauna@abv.bg>
2022-02-20 21:36:12 +0200

    fixed problems with boilerplate

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
+/* eslint-env node */
 module.exports = {
-    preset: "ts-jest",
-    testEnvironment: "node",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testPathIgnorePatterns: ["<rootDir>/.next/', '<rootDir>/node_modules/"],
+  moduleNameMapper: {
+    "\\.(scss|sass|css)$": "identity-obj-proxy",
+  },
 };
- 
--- src/__tests__/sample.test.ts
+++ src/__tests__/sample.test.ts
@@ -1,4 +1,5 @@
-describe("sample test", () => {
-    expect(1 + 1).toBe(2);
+import sum from "./sample";
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(1 + 2).toBe(3);
 });
-export { };
