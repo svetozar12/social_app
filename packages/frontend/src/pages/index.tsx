4d5aa2c25cb23f02fe77a7f2d8dd3ed874839842
svetozar12 <zaauna@abv.bg>
2022-03-20 23:54:17 +0200

    insane express structure

--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -1,24 +1,21 @@
 import React from "react";
-import { DatePicker, message, Alert } from "antd";
-function Home() {
-  const [date, setDate] = React.useState<any>(null);
-  const handleChange = (value: any) => {
-    message.info(
-      `Selected Date: ${value ? value.format("YYYY-MM-DD") : "None"}`
-    );
+import { message, Alert } from "antd";
+import axios from "axios";
 
-    setDate(value);
+function Home() {
+  const [article, setArticle] = React.useState([]);
+  const handleChange = async () => {
+    const res = await axios.get("http://localhost:8001/articles");
+    // setArticle(res);
+    console.log(res);
   };
+
+  React.useEffect(() => {
+    handleChange();
+  }, []);
   return (
     <div style={{ width: 400, margin: "100px auto" }}>
-      <DatePicker onChange={handleChange} />
-      <div style={{ marginTop: 16 }}>
-        Selected Date: {date ? date.format("YYYY-MM-DD") : "None"}
-      </div>
-      <Alert
-        message="Selected Date"
-        description={date ? date.format("YYYY-MM-DD") : "None"}
-      />
+      <Alert message="Selected Date" />
     </div>
   );
 }
