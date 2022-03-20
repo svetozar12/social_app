e5836025088cee1cb4a7f35e51f8e3a8d182d8af
svetozar12 <zaauna@abv.bg>
2022-03-21 00:11:57 +0200

    .

--- packages/backend/dist/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express = require("express");
-var routes_1 = require("./routes");
-// Boot express
-var app = express();
-// Application routing
-routes_1.routes(app);
-exports.default = app;
--- packages/backend/dist/utils/isNumber.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isNumber = void 0;
-exports.isNumber = function (n) {
-    return !isNaN(parseFloat(n)) && isFinite(n);
-};
